f60c072aa80ab67679972f06b43fecf0dce89e08
urmisaha <urmisaha@frgslave2.cse.iitb.ac.in>
2018-02-23 00:02:06 +0530

    First commit

---
+++ TestRun_file.py
@@ -0,0 +1,36 @@
+__author__ = 'urmi and minali'
+
+import unittest
+import LoginWrongCredentialsTest
+import LoginCorrectCredentialsTest
+import os
+import HTMLTestRunner
+
+direct = os.getcwd() + "\HTMLRunner"
+
+class MyTestSuite(unittest.TestCase):
+
+    def test_Issue(self):
+
+        login_test = unittest.TestSuite()
+        login_test.addTests([
+            unittest.defaultTestLoader.loadTestsFromTestCase(LoginCorrectCredentialsTest.LoginCorrect),
+            unittest.defaultTestLoader.loadTestsFromTestCase(LoginWrongCredentialsTest.LoginWrong),
+        ])
+
+        outfile = open(direct + "\TestReport.html", "w")
+
+        runner1 = HTMLTestRunner.HTMLTestRunner(
+            stream=outfile,
+            title='Test Report',
+            description='Login Tests'
+        )
+
+        runner1.run(login_test)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
