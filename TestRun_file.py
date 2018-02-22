01b5a1c31f0dd36b8072831685c69868bdbaf5a3
urmisaha <urmisaha@frgslave2.cse.iitb.ac.in>
2018-02-23 00:33:10 +0530

    shell script for python script run

--- TestRun_file.py
+++ TestRun_file.py
@@ -10,27 +10,26 @@ import HTMLTestRunner
 
 class MyTestSuite(unittest.TestCase):
 
-    def test_Issue(self):
+	def test_Issue(self):
+		login_test = unittest.TestSuite()
+		login_test.addTests([
+			unittest.defaultTestLoader.loadTestsFromTestCase(LoginCorrectCredentialsTest.LoginCorrect),
+			unittest.defaultTestLoader.loadTestsFromTestCase(LoginWrongCredentialsTest.LoginWrong),
+		])
 
-        login_test = unittest.TestSuite()
-        login_test.addTests([
-            unittest.defaultTestLoader.loadTestsFromTestCase(LoginCorrectCredentialsTest.LoginCorrect),
-            unittest.defaultTestLoader.loadTestsFromTestCase(LoginWrongCredentialsTest.LoginWrong),
-        ])
+		outfile = open("TestReport.html", "w")
 
-        outfile = open("TestReport.html", "w")
+		runner1 = HTMLTestRunner.HTMLTestRunner(
+			stream=outfile,
+			title='Test Report',
+			description='Login Tests'
+		)
 
-        runner1 = HTMLTestRunner.HTMLTestRunner(
-            stream=outfile,
-            title='Test Report',
-            description='Login Tests'
-        )
-
-        runner1.run(login_test)
+		runner1.run(login_test)
 
 
 
 
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
---
+++ testrun.sh
@@ -0,0 +1,1 @@
+python3 TestRun_file.py
